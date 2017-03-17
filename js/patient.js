aea60a72d5694ddd85f93070668e890367361db8
Keely Glenn <glenj392@newschool.edu>
2017-03-17 10:39:56 -0700

    add for loop to loop through response and return list of practices

--- js/patient.js
+++ js/patient.js
@@ -16,12 +16,9 @@ function Patient(){
 Patient.prototype.getDoctors = function(medicalIssue) {
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
     .then(function(response) {
-        response.data.forEach(function(data){
-        // displayDoctor(data.profile);
-        console.log(data.profile.first_name);
-        // console.log(data.profile.last_name);
-      });
-      })
+      for(var i=0; i< response.data.length; i ++)
+    console.log(response.data[i].practices[0].name);
+    })
      .fail(function(error){
         console.log("fail");
       });
